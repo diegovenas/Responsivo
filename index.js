0b6a1eea66ff27ad261dbf552bedfd5155f8241b
diegovenas <diegovenas61@gmail.com>
2022-06-28 20:28:07 -0300

    Criacao do modal

---
+++ index.js
@@ -0,0 +1,9 @@
+const abrirModal = () => {
+    document.querySelector('.containerModal').style.display = "flex";
+  };
+
+
+  const fecharModal = () => {
+    document.querySelector('.containerModal').style.display = "none";
+  };
+
